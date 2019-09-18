8ded0a39cc6bac7df3ba8684846eed7bfc460036
mini536 <32984303+mini536@users.noreply.github.com>
2019-09-18 00:39:11 -0500

    test
    web

---
+++ 233.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name 网页调试
+// @description 有限的为手机浏览器提供调试网页
+// @match *://*/*
+// @author QQ大冰
+// @charset UTF-8
+// @version 1.0
+// ==/UserScript==
+javascript:(function () { var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); script.onload = function () { eruda.init() } })();
