1b6dee9832b2d8056b4c2c39cd1aeaadd8b7ce04
mini536 <32984303+mini536@users.noreply.github.com>
2019-09-18 05:42:21 +0000

    Update 233.user.js

--- 233.user.js
+++ 233.user.js
@@ -6,4 +6,4 @@
 // @charset UTF-8
 // @version 1.0
 // ==/UserScript==
-javascript:(function () { var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); script.onload = function () { eruda.init() } })();
+javascript:(function () { var script = document.createElement('script'); script.src="//raw.githubusercontent.com/logoove/weui/master/js/eruda.js"; document.body.appendChild(script); script.onload = function () { eruda.init() } })();
