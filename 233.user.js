59b69b9f275b5bc47401401198b6278367eff0c8
mini536 <32984303+mini536@users.noreply.github.com>
2020-04-19 18:19:23 +0800

    Delete 233.user.js

--- 233.user.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name 网页调试
-// @description 有限的为手机浏览器提供调试网页
-// @match *://*/*
-// @author QQ大冰
-// @charset UTF-8
-// @version 1.0
-// ==/UserScript==
-javascript:(function () { var script = document.createElement('script'); script.src="//raw.githubusercontent.com/mini536/dadgb/master/2333.js"; document.body.appendChild(script); script.onload = function () { eruda.init() } })();
